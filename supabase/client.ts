a5b30e1fd94db9d69dee98112d13403e332ddc96
Bruno Galvão <bruno_galvao@outlook.com>
2025-05-09 09:05:21 -0300

    :star: fix client

--- supabase/client.ts
+++ supabase/client.ts
@@ -11,7 +11,7 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-const url = import.meta.env.SUPABASE_URL || process.env.SUPABASE_URL!;
-const key = import.meta.env.SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY!;
+const url = process.env.SUPABASE_URL!;
+const key = process.env.SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(url, key);
