ca02f242ee5e71a71a49f8d90b6c7a693cc9c8d6
Bruno Galvão <bruno_galvao@outlook.com>
2025-05-27 13:27:39 -0300

    :up: add metodo delete

--- api/tasks/[id].ts
+++ api/tasks/[id].ts
@@ -6,8 +6,10 @@ export const config = { runtime: "edge" };
 
 const app = new Hono();
 
+// CORS
 app.options("/api/tasks/:id", () => handleOptions());
 
+// PUT /api/tasks/:id
 app.put("/api/tasks/:id", async (c) => {
   const id = c.req.param("id");
   const body = await c.req.json();
@@ -37,5 +39,37 @@ app.put("/api/tasks/:id", async (c) => {
   return c.json(data[0]);
 });
 
+// DELETE /api/tasks/:id
+app.delete("/api/tasks/:id", async (c) => {
+  const id = c.req.param("id");
+
+  const token = c.req.header("Authorization");
+  const supabase = createClientWithAuth(token);
+
+  const {
+    data: { user },
+    error: authError,
+  } = await supabase.auth.getUser();
+
+  if (authError || !user) {
+    return c.json({ error: "Usuário não autenticado." }, 401);
+  }
+
+  const { data, error } = await supabase
+    .from("tasks")
+    .delete()
+    .eq("id", id)
+    .eq("user_id", user.id)
+    .select();
+
+  if (error) return c.json({ error: error.message }, 500);
+  if (!data.length)
+    return c.json({ error: "Tarefa não encontrada ou acesso negado." }, 404);
+
+  return c.json({ message: "Tarefa deletada com sucesso." });
+});
+
+// Exports
 export const OPTIONS = app.fetch;
 export const PUT = app.fetch;
+export const DELETE = app.fetch;
