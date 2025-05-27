db0d199e74cf526fe40e49403be170c816e76769
Bruno Galvão <bruno_galvao@outlook.com>
2025-05-27 14:23:27 -0300

    :star: patch fix

--- api/config/apiHeader.ts
+++ api/config/apiHeader.ts
@@ -1,6 +1,6 @@
 export const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS", // ✅ inclui PATCH
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
