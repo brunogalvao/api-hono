dec333e078c1b553268f05e5bb2d1d24ef8bcf7c
Bruno Galvão <bruno_galvao@outlook.com>
2025-05-27 13:00:40 -0300

    :hammer: fix cors

--- api/config/apiHeader.ts
+++ api/config/apiHeader.ts
@@ -1,10 +1,9 @@
 export const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, OPTIONS, DELETE",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
-// Middleware CORS padrão para OPTIONS
 export function handleOptions(): Response {
   return new Response(null, {
     status: 204,
