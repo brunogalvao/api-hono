bcfa803b50727afe9c0ea293f591bebbf9062a38
Bruno Galvão <bruno_galvao@outlook.com>
2025-06-10 10:48:51 -0300

    :up: change client

--- api/config/creatClient.ts
+++ api/config/creatClient.ts
@@ -1,16 +1,29 @@
-import { createClient as createSupabaseClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
-// Função para criar client dinâmico com o token do usuário
-export function createClientWithAuth(token?: string) {
-  return createSupabaseClient(
-    process.env.SUPABASE_URL!,
+// export const createClientWithAuth = (token?: string | null) => {
+//   return createClient(
+//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//     process.env.SUPABASE_ANON_KEY!,
+//     {
+//       global: {
+//         headers: {
+//           Authorization: `Bearer ${token}`,
+//         },
+//       },
+//     },
+//   );
+// };
+
+export const createClientWithAuth = (token?: string | null) => {
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_ANON_KEY!,
     {
       global: {
         headers: {
-          Authorization: token ?? "",
+          Authorization: `Bearer ${token}`,
         },
       },
     },
   );
-}
+};
